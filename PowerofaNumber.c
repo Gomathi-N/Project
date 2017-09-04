632c22730b4b70ce2000f5df2fc1c6fbc7c6f881
Gomathi-N <ngomathi.cs@gmail.com>
2017-09-04 11:26:16 +0530

    Create PowerofaNumber.c

---
+++ PowerofaNumber.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int power(int base, int exponent);
+
+int main()
+{
+    int b, e;
+    scanf("%d%d",&b,&e);
+    int result = power(b,e);
+    printf("%d", result);
+    return 0;
+}
+int power(int base, int exponent){
+    int result = base;
+    for(int i = 1; i<exponent; i++){
+        result = result * base;
+    }
+    return result;
+}
